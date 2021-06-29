65ac9670cb9c96b15df8c9adbf77d894fde0100b
retricsu <inneverland2013@gmail.com>
2021-06-29 07:15:32 +0200

    change from class to fix type err

--- src/hardhat/wallet-signer.ts
+++ src/hardhat/wallet-signer.ts
@@ -1,6 +1,5 @@
-import { Wallet } from "@ethersproject/wallet";
+import { Wallet } from "ethers";
 import { Provider, TransactionRequest } from "@ethersproject/abstract-provider";
-import { JsonRpcProvider } from "@ethersproject/providers";
 import { ExternallyOwnedAccount } from "@ethersproject/abstract-signer";
 import { SigningKey } from "@ethersproject/signing-key";
 import { resolveProperties } from "@ethersproject/properties";
@@ -22,7 +21,7 @@ export default interface PolyjuiceWallet extends Wallet {
   constructor(
     privateKey: BytesLike | ExternallyOwnedAccount | SigningKey,
     polyjuiceConfig: PolyjuiceConfig,
-    provider?: JsonRpcProvider
+    provider?: Provider
   );
 }
 
@@ -33,7 +32,7 @@ export default class PolyjuiceWallet extends Wallet {
   constructor(
     privateKey: BytesLike | ExternallyOwnedAccount | SigningKey,
     polyjuiceConfig: PolyjuiceConfig,
-    provider?: JsonRpcProvider
+    provider?: Provider
   ) {
     super(privateKey, provider);
     const { web3RpcUrl, abiItems, godwokerOption } = polyjuiceConfig;
