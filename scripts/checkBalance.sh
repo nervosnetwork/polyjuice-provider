7b2445398b28a6c371afe6ad85146e908ffa0d31
RetricSu <inneverland2013@gmail.com>
2021-11-18 15:12:48 +0800

    fix(ci): checkBalance result single quote

--- scripts/checkBalance.sh
+++ scripts/checkBalance.sh
@@ -6,7 +6,7 @@ if [[ $s =~ '"jsonrpc":"2.0"' ]]; then
 else echo "call getBalance rpc failed" && exit 137
 fi
 
-if [[ $s =~ "{"jsonrpc":"2.0","id":1,"result":"0x0"}" ]]; then
+if [[ $s =~ '{"jsonrpc":"2.0","id":1,"result":"0x0"}' ]]; then
         curl $POLYJUICE_URL/deposit?eth_address=$TEST_ETH_ADDRESS
 else echo "account already initialed. skip. getBalance Result: $s."
 fi
