1dad6fefef98b2715338b3568f27a79d88bebbdc
Daniel Kmak <neverkuzi@gmail.com>
2022-01-27 23:57:09 +0100

    fix: fix bytes abi decoding

--- packages/base/src/abi.ts
+++ packages/base/src/abi.ts
@@ -233,6 +233,7 @@ export function decodeInputDataByAbi(data: HexString, abiItem: AbiItem) {
     const isUint = abiItem.inputs[i].type.indexOf("uint") === 0;
     const isInt = abiItem.inputs[i].type.indexOf("int") === 0;
     const isAddress = abiItem.inputs[i].type.indexOf("address") === 0;
+    const isBytes = abiItem.inputs[i].type.indexOf("bytes") === 0;
 
     if (isUint || isInt) {
       const isArray = Array.isArray(param);
@@ -253,6 +254,10 @@ export function decodeInputDataByAbi(data: HexString, abiItem: AbiItem) {
       } else {
         parsedParam = param.toLowerCase();
       }
+    }
+
+    if (isBytes) {
+      parsedParam = parsedParam === null ? '0x' : parsedParam;
     }
 
     retData.params.push({
@@ -391,7 +396,7 @@ export class Abi {
 
       retData.params.push({
         name: abiItem.inputs[i].name,
-        value: parsedParam,
+        value: parsedParam === null ? "0x" : parsedParam,
         type: abiItem.inputs[i].type,
       });
     }
